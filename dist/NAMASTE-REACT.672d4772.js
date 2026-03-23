dbd639b2c66800d9fda1dde2f0fad6ba007a2f99
Shivacharan013 <shivacharan013@gmail.com>
2026-03-23 21:19:15 +0530

    Remove .parcel-cache and dist folders from tracking

--- dist/NAMASTE-REACT.672d4772.js
+++
@@ -1,7 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "heading"
-}, "Namaste React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=NAMASTE-REACT.672d4772.js.map
