7210b36b1662dccccf0a1aa3dc84a59d383e7e19
Shivacharan013 <shivacharan013@gmail.com>
2026-03-23 21:15:56 +0530

    Remove node modules from tracking

---
+++ dist/NAMASTE-REACT.672d4772.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Namaste React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=NAMASTE-REACT.672d4772.js.map
