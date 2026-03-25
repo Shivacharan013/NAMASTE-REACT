f5432e282a27bef23d3a778b949d4ba23c438ece
Shivacharan013 <shivacharan013@gmail.com>
2026-03-25 19:57:54 +0530

    igniting the app

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+
+
+
+const Title=()=>(<h1 className="head" tabIndex="5">
+        Namaste React from React using JSX
+        </h1>);
+//React Functional Component
+const HeadingComponent = () => (
+        <div id="container">
+          <Title />
+          <h1 id="heading">Hello World from React using Functional Component</h1>
+        </div>
+      );
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
